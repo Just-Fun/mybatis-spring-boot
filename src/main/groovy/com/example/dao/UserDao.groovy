5841097f1201c7cc46d747a8671735f93bc0081c
Sergii Zagryvyi <serzhcello@gmail.com>
2017-10-13 19:52:11 +0300

    add methods and tests

--- src/main/groovy/com/example/dao/UserDao.groovy
+++ src/main/groovy/com/example/dao/UserDao.groovy
@@ -1,6 +1,8 @@
 package com.example.dao
 
 import com.example.model.User
+import org.apache.ibatis.annotations.Delete
+import org.apache.ibatis.annotations.Insert
 import org.apache.ibatis.annotations.Mapper
 import org.apache.ibatis.annotations.Select
 
@@ -19,4 +21,30 @@ public interface UserDao {
 
     @Select("SELECT * FROM USERS")
     List<User> findAll();
+
+    @Select(""" SELECT ID, 
+                       FIRST_NAME   as firstName, 
+                       LAST_NAME    as lastName, 
+                       USER_NAME    as userName, 
+                       ACTIVE_SINCE as activeOn 
+                FROM USERS
+                WHERE ID = #{id} """)
+    User findOne(int id);
+
+    /*@Insert("INSERT into village(name,district) VALUES(#{villageName}, #{district})")
+	void insertVillage(Village village);
+
+	@Update("UPDATE village SET name=#{villageName}, district =#{district} WHERE id =#{vid}")
+	void updateVillage(Village village);
+
+	@Delete("DELETE FROM village WHERE id =#{id}")
+	void deleteVillage(int id);*/
+
+    @Insert(""" INSERT INTO USERS
+                    (USER_NAME, FIRST_NAME, LAST_NAME, ACTIVE_SINCE )        
+                    VALUES(#{userName}, #{firstName}, #{lastName}, #{activeOn}) """)
+    void save(User user);
+
+    @Delete("DELETE FROM USERS WHERE ID=#{id}")
+    void delete(int id);
 }
--- src/test/java/com/example/DemoTest.java
+++ src/test/java/com/example/DemoTest.java
@@ -4,13 +4,18 @@ import com.example.dao.PublisherMapper;
 import com.example.dao.UserDao;
 import com.example.model.Publisher;
 import com.example.model.User;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.not;
@@ -18,35 +23,82 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoTest {
 
-  @Autowired
-  PublisherMapper publisherMapper;  // Mybatis generated bean
+    @Autowired
+    PublisherMapper publisherMapper;  // Mybatis generated bean
 
-  @Autowired
-  UserDao userDao;
+    @Autowired
+    UserDao userDao;
 
-  @Test
-  public void findPublishers() {
-    System.out.println( publisherMapper.getClass().getName());
-    List<Publisher> publishers = publisherMapper.findAll();
-    for (Publisher publisher : publishers) {
-      System.out.println(publisher);
-      assertThat(publisher.getName(), is(not(nullValue())));
+    @Test
+    public void findPublishers() {
+        System.out.println(publisherMapper.getClass().getName());
+        List<Publisher> publishers = publisherMapper.findAll();
+        for (Publisher publisher : publishers) {
+            System.out.println(publisher);
+            assertThat(publisher.getName(), is(not(nullValue())));
+        }
+        assertThat(publishers.size(), is(greaterThan(0)));
     }
-    assertThat(publishers.size(), is(greaterThan(0)));
-  }
 
-  @Test
-  public void findsSomeUsers() {
-    List<User> allUsers = userDao.findOrderedUsers();
-    for (User user : allUsers) {
-      System.out.println(user);
-      assertThat(user.getUserName(), is(not(nullValue())));
+    @Test
+    public void findsSomeUsers() {
+        List<User> allUsers = userDao.findOrderedUsers();
+        for (User user : allUsers) {
+            System.out.println(user);
+            assertThat(user.getUserName(), is(not(nullValue())));
+        }
+        assertThat(allUsers.size(), is(greaterThan(0)));
+        assertEquals(4, allUsers.size());
     }
-    assertThat(allUsers.size(), is(greaterThan(0)));
-  }
+
+    @Test
+    public void findsOneUser() {
+        User user = userDao.findOne(1);
+        assertEquals(user.getUserName(), "credmond");
+    }
+
+    @Test
+    @Transactional
+    public void saveUsers() throws ParseException {
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Date date = formatter.parse("10/13/2017");
+
+        User chuckNorris = User.builder()
+                .userName("chuck")
+                .firstName("Chuck")
+                .lastName("Norris")
+                .activeOn(date)
+                .build();
+
+        userDao.save(chuckNorris);
+
+        List<User> users = userDao.findOrderedUsers();
+        User chuck = users.stream()
+                .filter(user -> user.getUserName().equals("chuck"))
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(chuck);
+        assertEquals(chuck.getLastName(), "Norris");
+
+    }
+
+    @Test
+    @Transactional
+    public void deleteUser() throws ParseException {
+        userDao.delete(2);
+
+        List<User> allUsers = userDao.findOrderedUsers();
+
+        assertEquals(allUsers.size(), 3);
+
+    }
+
 }
