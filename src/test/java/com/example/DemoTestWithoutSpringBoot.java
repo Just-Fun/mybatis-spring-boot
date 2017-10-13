404371ae7cded06e5a310290d39c9ce7bb873b87
Sergii Zagryvyi <serzhcello@gmail.com>
2017-10-13 18:56:32 +0300

    add DemoTestWithoutSpringBoot

---
+++ src/test/java/com/example/DemoTestWithoutSpringBoot.java
@@ -0,0 +1,53 @@
+package com.example;
+
+import com.example.configuration.H2TestConfiguration;
+import com.example.dao.PublisherMapper;
+import com.example.dao.UserDao;
+import com.example.model.Publisher;
+import com.example.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.core.Is.is;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {H2TestConfiguration.class})
+public class DemoTestWithoutSpringBoot {
+
+  @Autowired
+  PublisherMapper publisherMapper;  // Mybatis generated bean
+
+  @Autowired
+  UserDao userDao;
+
+  @Test
+  public void findPublishers() {
+    System.out.println( publisherMapper.getClass().getName());
+    List<Publisher> publishers = publisherMapper.findAll();
+    for (Publisher publisher : publishers) {
+      System.out.println(publisher);
+      assertThat(publisher.getName(), is(not(nullValue())));
+    }
+    assertThat(publishers.size(), is(greaterThan(0)));
+  }
+
+  @Test
+  public void findsSomeUsers() {
+    List<User> allUsers = userDao.findOrderedUsers();
+    for (User user : allUsers) {
+      System.out.println(user);
+      assertThat(user.getUserName(), is(not(nullValue())));
+    }
+    assertThat(allUsers.size(), is(greaterThan(0)));
+  }
+}
---
+++ src/test/java/com/example/configuration/H2TestConfiguration.java
@@ -0,0 +1,13 @@
+package com.example.configuration;
+
+import com.example.DemoApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * @author sergii.zagryvyi on 13.10.2017
+ */
+@Configuration
+@Import(DemoApplication.class)
+public class H2TestConfiguration {
+}
